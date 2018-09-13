a19aa046b9b988d5b429543639475944143668bf
TrentPearson <trent.pearson172@hotmail.com>
2018-09-13 16:02:20 +1000

    Background image updated

---
+++ ResumeBuilder/Login.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ResumeBuilder
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            #if _Android_
+                TranslationY = 
+        }
+
+    }
+}
---
+++ ResumeBuilder/View/Button.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ResumeBuilder.View
+{
+    public partial class Button : ContentPage
+    {
+        public Button()
+        {
+            InitializeComponent();
+        }
+    }
+}
